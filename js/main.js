9531e3d9b36ca01514fafc45fe6f2589f7299d24
azermurad1i <azermuradli1@gmail.com>
2019-08-31 00:10:22 +0500

    fixes

--- js/main.js
+++ js/main.js
@@ -26,3 +26,42 @@ lists.forEach(list=>{
     })
     
 })
+// profile
+let profileLists=document.querySelectorAll(".profile-list")
+let profileContents=document.querySelectorAll(".profile-content")
+
+
+    
+profileLists.forEach(list=>{
+    list.addEventListener("click",()=>{
+        if(list.dataset.id===6) {
+
+        }
+        else {
+            let acID=document.querySelector(".profile-lists .active").dataset.id
+            document.querySelectorAll(".profile-content")[acID].classList.remove('d-block')
+            document.querySelectorAll(".profile-content")[acID].classList.add('d-none')
+            document.querySelectorAll(".profile-content")[acID].classList.remove('active')
+            document.querySelector(".profile-lists .active").classList.remove("border-success")
+            document.querySelector(".profile-lists .active").children[0].children[0].classList.remove("text-success")
+            document.querySelector(".profile-lists .active").children[0].children[1].classList.remove("text-success")
+            document.querySelector(".profile-lists .active").children[0].children[0].classList.add("text-muted")
+            document.querySelector(".profile-lists .active").children[0].children[1].classList.add("text-muted")
+            document.querySelector(".profile-lists .active").classList.remove("active")
+            list.classList.add("active")
+            list.classList.add("border-success")
+            list.classList.remove("border-muted")
+            list.children[0].children[0].classList.add("text-success")
+            list.children[0].children[1].classList.add("text-success")
+            list.children[0].children[0].classList.remove("text-muted")
+            list.children[0].children[1].classList.remove("text-muted")
+            let id=list.dataset.id
+            document.querySelectorAll(".profile-content")[id].classList.add('d-block')
+            document.querySelectorAll(".profile-content")[id].classList.remove('d-none')
+            document.querySelectorAll(".profile-content")[id].classList.add('active')
+        }
+       
+
+        
+    })
+})
