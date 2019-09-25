c74e52340ac50cf7888e2feb2481f1678afc8d80
hemidsultan <sultanzade.06@mail.ru>
2019-09-25 21:32:58 +0400

    feedbacks from db is done, without photo

---
+++ src/main/java/com/bsptech/teachermanagement/service/inter/FeedbackServiceInter.java
@@ -0,0 +1,11 @@
+package com.bsptech.teachermanagement.service.inter;
+
+import com.bsptech.teachermanagement.entity.Feedback;
+
+import java.util.List;
+
+public interface FeedbackServiceInter {
+
+    List<Feedback> findAll();
+
+}
