1b0019b9d84e352fa477d046a4837ca98e7e99e2
Goshgar Mirzayev <35040732+goshgarmirzayev@users.noreply.github.com>
2019-09-28 01:05:58 +0400

    Update SupportServiceInter.java

--- src/main/java/com/bsptech/teachermanagement/service/inter/SupportServiceInter.java
+++ src/main/java/com/bsptech/teachermanagement/service/inter/SupportServiceInter.java
@@ -1,6 +1,7 @@
 package com.bsptech.teachermanagement.service.inter;
 
 import com.bsptech.teachermanagement.entity.Support;
+import org.springframework.stereotype.Service;
 @Service
 public interface SupportServiceInter {
     public Support save(Support support);
