3690d0226bb8522d1803be8e6ece555d27835500
Shahriyar <shahriyarjalilov@gmail.com>
2019-10-07 19:38:04 +0500

    Lesson

--- src/main/java/com/bsptech/teachermanagement/service/inter/SupportServiceInter.java
+++ src/main/java/com/bsptech/teachermanagement/service/inter/SupportServiceInter.java
@@ -1,6 +1,8 @@
 package com.bsptech.teachermanagement.service.inter;
 
 import com.bsptech.teachermanagement.entity.Support;
+import org.springframework.stereotype.Service;
+
 @Service
 public interface SupportServiceInter {
     public Support save(Support support);
