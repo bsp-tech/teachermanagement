21937165e3e190ceb0470162676f7799d76688c9
muradisgandar <mr.murad157@mail.ru>
2019-11-20 00:35:45 +0400

    TASK-37 70% done

--- src/main/java/com/bsptech/teachermanagement/controller/admin/WebsiteSettingsController.java
+++ src/main/java/com/bsptech/teachermanagement/controller/admin/WebsiteSettingsController.java
@@ -7,10 +7,14 @@ package com.bsptech.teachermanagement.controller.admin;
 
 import com.bsptech.teachermanagement.dao.WebsiteSettingDataInter;
 import com.bsptech.teachermanagement.entity.WebsiteSettings;
+import java.util.Date;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
@@ -27,12 +31,23 @@ public class WebsiteSettingsController {
     @GetMapping("/setting")
     public ModelAndView page(ModelAndView modelAndView) {
         Iterable<WebsiteSettings> settings = websiteSettingDataInter.findAll();
-        
-        
-        
         modelAndView.addObject("settings",settings);
         modelAndView.setViewName("admin/websitesetting");
         return modelAndView;
     }
+    
+    
+    @PostMapping("/add")
+    public ModelAndView add(@ModelAttribute("admin") WebsiteSettings websiteSettings,ModelAndView modelAndView) {
+        
+        websiteSettings.setInsertDateTime(new java.sql.Date(new Date().getTime()));
+        websiteSettingDataInter.save(websiteSettings);
+        modelAndView.setViewName("redirect:/admin/setting");
+        return modelAndView;
+    }
+    
+    
+    
+    
     
 }
