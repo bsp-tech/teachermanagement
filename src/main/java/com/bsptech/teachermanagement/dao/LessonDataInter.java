41603fc0ad8878595444694429a2cf28d31784a3
muradisgandar <mr.murad157@mail.ru>
2019-11-24 23:14:46 +0400

    update and search operation is done

--- src/main/java/com/bsptech/teachermanagement/dao/LessonDataInter.java
+++ src/main/java/com/bsptech/teachermanagement/dao/LessonDataInter.java
@@ -1,8 +1,12 @@
 package com.bsptech.teachermanagement.dao;
 
 import com.bsptech.teachermanagement.entity.Lesson;
+import java.util.List;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
-public interface LessonDataInter extends CrudRepository<Lesson,Integer> {
+public interface LessonDataInter extends CrudRepository<Lesson, Integer> {
 
+    @Query(value = "select l from Lesson l where l.name=?1 or l.about=?2 or l.thumbnailPath=?3")
+    public List<Lesson> getLessonsForSearching(String name,String about,String thumbnailPath);
 }
