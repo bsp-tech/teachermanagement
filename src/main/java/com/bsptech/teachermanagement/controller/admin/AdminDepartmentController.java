ed320b146286a2af0131e053b30316567371a120
muradisgandar <mr.murad157@mail.ru>
2019-12-07 16:18:26 +0400

    department page for admin is done

---
+++ src/main/java/com/bsptech/teachermanagement/controller/admin/AdminDepartmentController.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bsptech.teachermanagement.controller.admin;
+
+import com.bsptech.teachermanagement.dao.DepartmentDataInter;
+import com.bsptech.teachermanagement.entity.Department;
+import java.util.Date;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ *
+ * @author murad_isgandar
+ */
+@Controller
+@RequestMapping("/admin/departments")
+public class AdminDepartmentController {
+    
+    @Autowired
+    private DepartmentDataInter departmentDataInter;
+    
+    @GetMapping
+    public ModelAndView page(ModelAndView modelAndView) {
+        
+        modelAndView.addObject("departments",departmentDataInter.findAll());
+        modelAndView.setViewName("admin/departments");
+        return modelAndView;
+    }
+    
+    @PostMapping(value = "/add")
+    public ModelAndView add(@ModelAttribute("department") Department department) {
+        
+        department.setInsertDateTime(new java.sql.Date(new Date().getTime()));
+        departmentDataInter.save(department);
+        return new ModelAndView("redirect:/admin/departments");
+    }
+    
+    @PostMapping(value = "/update")
+    public ModelAndView update(@RequestParam(value = "id")Integer id,@RequestParam("name") String name) {
+        
+        if(name!=null && !name.isEmpty()){
+            
+         Optional<Department> result =  departmentDataInter.findById(id);
+         Department department = result.get();
+         
+         department.setName(name);
+         department.setLastUpdateDateTime(new java.sql.Date(new Date().getTime()));
+         departmentDataInter.save(department);
+            
+        }
+        
+        return new ModelAndView("redirect:/admin/departments");
+    }
+    
+    @PostMapping(value = "/delete")
+    public ModelAndView delete(@RequestParam(value = "id") Integer id) {
+        
+        departmentDataInter.deleteById(id);
+        return new ModelAndView("redirect:/admin/departments");
+    }
+    
+    
+    
+    
+}
