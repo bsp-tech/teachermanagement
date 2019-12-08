6c6c3a0ceac09b211a62471de5c0545727e33ee0
hemidsultan <sultanzade.06@mail.ru>
2019-12-08 17:03:06 +0400

    supports

--- src/main/java/com/bsptech/teachermanagement/dao/SupportDataInter.java
+++ src/main/java/com/bsptech/teachermanagement/dao/SupportDataInter.java
@@ -2,7 +2,11 @@ package com.bsptech.teachermanagement.dao;
 
 import com.bsptech.teachermanagement.entity.Support;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
 public interface SupportDataInter extends CrudRepository<Support, Integer> {
 
+    List<Support> findByOrderByStatusIdIdDescInsertDateTimeAsc();
 
 }
