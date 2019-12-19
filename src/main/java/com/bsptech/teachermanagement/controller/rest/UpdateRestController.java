5e4681643cbf5355c030769a0a462293ab8b26dd
hemidsultan <sultanzade.06@mail.ru>
2019-12-19 21:30:49 +0400

    feedback update done

--- src/main/java/com/bsptech/teachermanagement/controller/rest/UpdateRestController.java
+++ src/main/java/com/bsptech/teachermanagement/controller/rest/UpdateRestController.java
@@ -1,12 +1,10 @@
 package com.bsptech.teachermanagement.controller.rest;
 
 import com.bsptech.teachermanagement.dao.FAQDataInter;
+import com.bsptech.teachermanagement.dao.FeedbackDataInter;
 import com.bsptech.teachermanagement.dao.FileDataInter;
 import com.bsptech.teachermanagement.dao.VideoDataInter;
-import com.bsptech.teachermanagement.entity.FAQ;
-import com.bsptech.teachermanagement.entity.File;
-import com.bsptech.teachermanagement.entity.Post;
-import com.bsptech.teachermanagement.entity.Video;
+import com.bsptech.teachermanagement.entity.*;
 import com.bsptech.teachermanagement.service.inter.PostServiceInter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -30,6 +28,9 @@ public class UpdateRestController {
 
     @Autowired
     VideoDataInter videoDataInter;
+
+    @Autowired
+    FeedbackDataInter feedbackDataInter;
 
     @RequestMapping("/blog/{id}")
     public Map<String, String> getPostDetails(@PathVariable("id") Integer id) {
@@ -68,6 +69,15 @@ public class UpdateRestController {
         data.put("header",video.getHeader());
         data.put("url",video.getUrl());
         data.put("sectionId",video.getSectionId().getId());
+        return data;
+    }
+
+    @RequestMapping("/feedback/{id}")
+    public Map<String, String> getFeedbackDetails(@PathVariable("id") Integer id){
+        Feedback feedback = feedbackDataInter.findById(id).get();
+        Map<String , String> data = new HashMap<>();
+        data.put("authorName", feedback.getAuthorName());
+        data.put("content",feedback.getContent());
         return data;
     }
 }
