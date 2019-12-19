1e775a09bf05527cd7002a6236ce7c0cd41bf004
hemidsultan <sultanzade.06@mail.ru>
2019-12-19 17:57:15 +0400

    faq update done

--- src/main/java/com/bsptech/teachermanagement/controller/rest/UpdateRestController.java
+++ src/main/java/com/bsptech/teachermanagement/controller/rest/UpdateRestController.java
@@ -1,5 +1,7 @@
 package com.bsptech.teachermanagement.controller.rest;
 
+import com.bsptech.teachermanagement.dao.FAQDataInter;
+import com.bsptech.teachermanagement.entity.FAQ;
 import com.bsptech.teachermanagement.entity.Post;
 import com.bsptech.teachermanagement.service.inter.PostServiceInter;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +18,9 @@ public class UpdateRestController {
     @Autowired
     PostServiceInter postServiceInter;
 
+    @Autowired
+    FAQDataInter faqDataInter;
+
     @RequestMapping("/blog/{id}")
     public Map<String, String> getPostDetails(@PathVariable("id") Integer id) {
         Post post = postServiceInter.findById(id);
@@ -24,6 +29,15 @@ public class UpdateRestController {
         data.put("thumbnailPath", post.getThumbnailPath());
         data.put("content", post.getContent());
         System.out.println("I am sended data");
+        return data;
+    }
+
+    @RequestMapping("/faq/{id}")
+    public Map<String, String> getFaqDetails(@PathVariable("id") Integer id){
+        FAQ faq = faqDataInter.findById(id).get();
+        Map<String, String> data = new HashMap<>();
+        data.put("header",faq.getHeader());
+        data.put("content",faq.getContent());
         return data;
     }
 }
